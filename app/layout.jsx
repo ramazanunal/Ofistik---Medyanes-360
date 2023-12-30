d8b840f09aa4df333ea4c9341bb5564648cceaf4
yusuf <ysfyldz0077@gmail.com>
2023-12-30 15:07:27 +0300

    redeploy

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,20 @@
+'use client';
 import '@/styles/globals.css';
 import {Provider} from 'react-redux';
 import {store} from '@/store/store';
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from 'react-toastify';
-import {useRouter} from "next/navigation";
 import Header from "@/containers/Home/_components/header";
 import Footer from "@/containers/Home/_components/footer";
-import {headers} from "next/headers";
 
 export default function RootLayout({children}) {
-
     return (
         <html lang='en' className='scroll-smooth focus:scroll-auto'>
         <body className='overflow-y-auto scroll-smooth overflow-x-hidden flex flex-col font-sans bg-primary'>
         <Provider store={store}>
             <Header/>
             {children}
-            <Footer/>
+            <Footer />
 
             {/* Toastify */}
             <ToastContainer
