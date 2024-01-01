d2cc8ff900ce5240040b8fa6d60c28ad0c72895d
yusuf <ysfyldz0077@gmail.com>
2024-01-01 14:48:40 +0300

    fix mobile dashboard logo

--- containers/dashboard/_components/Navbar.jsx
+++ containers/dashboard/_components/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className='flex w-full items-center h-fit p-2'>
-        <Image src="/terappinPurple.svg" width={100} height={50} objectFit="contain"/>
+        <Image src="/terappinPurple.svg" width={100} height={50} objectFit="contain" className="ml-4 md:ml-0"/>
     </nav>
   )
 }
