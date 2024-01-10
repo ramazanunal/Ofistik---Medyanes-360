764de933a1c6675eb4f22128c095b8a16e9eb219
yusuf <ysfyldz0077@gmail.com>
2024-01-11 00:06:54 +0300

    update search algorithm - user collect data

--- containers/Home/_components/header/Search_Algorithm/index.js
+++ containers/Home/_components/header/Search_Algorithm/index.js
@@ -1,33 +1,29 @@
-import levenshteinDistance from "./levenshtein";
+import levenshteinDistance from "@/containers/Home/_components/header/Search_Algorithm/levenshtein";
 
 const Search_Algorithm = (data, query, min_distance, keys, user_dt) => {
     const new_dt = [];
 
-    data.forEach((dt) => {
-        const distances = keys.reduce((acc, key) => {
-            const subKeys = dt[key].toLowerCase().split(" ");
-            let subDistances = subKeys.map((subKey) => {
-                return levenshteinDistance(subKey, query.toLowerCase());
-            });
+    for (let i = 0; i < data.length; i++) {
+        const dt = data[i];
+        const distances = [];
 
-            return acc.concat(subDistances);
-        }, []);
+        for (let j = 0; j < keys.length; j++) {
+            const subKeys = dt[keys[j]].toLowerCase().split(" ");
+            const subDistances = subKeys.map(subKey =>
+                levenshteinDistance(subKey, query.toLowerCase())
+            );
+            distances.push(...subDistances);
+        }
 
         let max_distance = Math.max(...distances);
 
-        const x = user_dt?.find(({ clicked }) => {
-            return clicked.every((key, idx) => {
-                return key === dt[keys[idx]]
-            })
-        })
+        const matchingUserDt = user_dt?.filter(({ clicked }) =>
+            clicked.every((key, idx) => key === dt[keys[idx]])
+        );
 
-        const every = x?.clicked?.every((key, idx) => {
-            return key === dt[keys[idx]]
-        })
-
-        if (every) {
-            max_distance += 0.1
-        }
+        matchingUserDt.forEach(() => {
+            max_distance += 0.1;
+        });
 
         if (max_distance >= min_distance) {
             new_dt.push({
@@ -35,7 +31,7 @@ const Search_Algorithm = (data, query, min_distance, keys, user_dt) => {
                 score: max_distance,
             });
         }
-    });
+    }
 
     new_dt.sort((a, b) => b.score - a.score);
 
