a7665809c1075df7783be75e14f0ff358f9c05b8
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-01-15 14:23:18 +0300

    providers added and css changed.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,13 @@
-'use client';
 import '@/styles/globals.css';
-import { Provider } from 'react-redux';
-import { store } from '@/store/store';
+import Providers from '@/providers';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import Header from '@/containers/Home/_components/header';
-import Footer from '@/containers/Home/_components/footer';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='scroll-smooth'>
-      <head>
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
-          integrity='sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=='
-          crossOrigin='anonymous'
-          referrerPolicy='no-referrer'
-        />
-      </head>
-
-      <body className='scroll-smooth overflow-x-hidden flex flex-col font-sans bg-primary'>
-        <Provider store={store}>
+    <html lang='en'>
+      <body>
+        <Providers>
           {children}
           {/* Toastify */}
           <ToastContainer
@@ -36,7 +22,7 @@ export default function RootLayout({ children }) {
             pauseOnHover
             theme='dark'
           />
-        </Provider>
+        </Providers>
       </body>
     </html>
   );
