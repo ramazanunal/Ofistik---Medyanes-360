d23d4ebf028b986defe203c02ce5cf44286fa839
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-01-15 14:08:28 +0300

    Next auth created.

---
+++ pages/api/auth/[...nextauth].jsx
@@ -0,0 +1,96 @@
+import prisma from '@/lib/prisma';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import DecryptPassword from '@/lib/decryptPassword';
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+
+      async authorize(credentials) {
+        try {
+          const { email, password } = credentials;
+
+          // email ve password boş ise hata fırlatıyoruz.
+          if (!email || !password) {
+            throw new Error('Please enter your email and password');
+          }
+
+          // email adresi veritabanında kayıtlı mı kontrol ediyoruz.
+          const findUser = await prisma.user.findUnique({
+            where: { email: email },
+          });
+
+          console.log(findUser);
+
+          // email adresi veritabanında kayıtlı değilse hata fırlatıyoruz.
+          if (!findUser) {
+            throw new Error('No user found');
+          }
+
+          // Veritabanındaki şifre ile giriş yapan kullanıcının girdiği şifreyi karşılaştırıyoruz.
+          const passwordDecrypt = await DecryptPassword(
+            password,
+            findUser.password
+          );
+
+          // Şifreler eşleşmiyorsa hata fırlatıyoruz.
+          if (!passwordDecrypt) {
+            throw new Error('Password is wrong');
+          }
+
+          // Kullanıcı bilgilerini döndürüyoruz.
+          const user = {
+            name: findUser.name,
+            email: findUser.email,
+            phone: findUser.phone,
+            role: findUser.role,
+          };
+
+          return user;
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    encryption: true,
+  },
+
+  // kullanıcı giriş yaptıktan sonra giriş yapan kullanıcının bilgilerini token değişkenine atıyoruz.
+  session: {
+    strategy: 'jwt',
+    maxAge: 1 * 24 * 60 * 60, // 1 days * 24 hours * 60 minutes * 60 seconds
+  },
+
+  callbacks: {
+    // jwt fonksiyonu ile kullanıcı giriş yaptıktan sonra giriş yapan kullanıcının bilgilerini token değişkenine atıyoruz.
+    // bu bilgileri session fonksiyonunda kullanacağız.
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = { ...user };
+      }
+      return token;
+    },
+
+    // Client Component içerisinde kullanılır.
+    async session({ session, token }) {
+      if (token?.user) {
+        session.user = token.user;
+      }
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
