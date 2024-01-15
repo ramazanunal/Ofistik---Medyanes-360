b92d549112650c4fef3bc937ea0b7aa774f5d4d6
Rahman Çaylak <rahmancaylakk@gmail.com>
2024-01-15 10:43:15 +0300

    password hash package and function added.

---
+++ lib/encryptPassword.jsx
@@ -0,0 +1,15 @@
+import bcrypt from 'bcryptjs';
+
+const EncryptPassword = async (password) => {
+  try {
+    if (!password) throw new Error('Error: Password is empty');
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    return password;
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+export default EncryptPassword;
