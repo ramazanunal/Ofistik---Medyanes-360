ee31e8077e62aa0476b052bcbdd732d5c5e525cb
yusuf <ysfyldz0077@gmail.com>
2024-01-18 13:38:23 +0300

    fix icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,11 @@ import {SessionProvider} from "next-auth/react";
 
 export default function RootLayout({children, session}) {
     return (<html lang='en'>
+    <header>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+              integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+              crossOrigin="anonymous" referrerpolicy="no-referrer"/>
+    </header>
     <body>
     <SessionProvider session={session}>
         {children}
