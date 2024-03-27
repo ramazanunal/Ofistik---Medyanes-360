d037af1da0a8e3a519cf194bb67c84dddfa6bd13
yusuf <ysfyldz0077@gmail.com>
2024-03-27 17:10:45 +0300

    Update socketio.js

--- pages/api/socketio.js
+++ pages/api/socketio.js
@@ -1,26 +1,23 @@
 import { Server } from 'socket.io'
 
 const ioHandler = (req, res) => {
-    if (!res.socket.server.io) {
-        console.log('*First use, starting socket.io')
+    console.log('*First use, starting socket.io')
 
-        const io = new Server(res.socket.server, {
-            cors: [
-                "*"
-            ],
+    const io = new Server(res.socket.server, {
+        cors: [
+            "*"
+        ],
+    })
+
+    io.on('connection', socket => {
+        socket.broadcast.emit('a user connected')
+        socket.on('hello', msg => {
+            socket.emit('hello', 'world!')
         })
+    })
 
-        io.on('connection', socket => {
-            socket.broadcast.emit('a user connected')
-            socket.on('hello', msg => {
-                socket.emit('hello', 'world!')
-            })
-        })
+    res.socket.server.io = io
 
-        res.socket.server.io = io
-    } else {
-        console.log('socket.io already running')
-    }
     res.send("Hello world")
 }
 
