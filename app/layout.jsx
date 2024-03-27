30a9a7a63a3af879552c4b45bdb083b45c00742a
yusuf <ysfyldz0077@gmail.com>
2024-03-27 15:51:44 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children, session }) {
         <body className=''>
             <SessionProvider session={session}>
                 <SocketContainer>
+                    
                     {children}
                 </SocketContainer>
                 {/* Toastify */}
