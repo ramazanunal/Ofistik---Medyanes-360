72bc51c334f8f5c24b38ba5040865660761c4ba8
Bayram Çınar <bayram200212@gmail.com>
2024-05-13 15:02:26 +0300

    agora

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-}
+  future: {
+    webpack5: true,
+  },
+  webpack: function (config, options) {
+    console.log(options.webpack.version);
+    config.experiments = {};
+    // Add options to webpack config
+    config.output.publicPath = "/";
+    return config;
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
