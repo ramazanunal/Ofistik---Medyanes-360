8f6bc847b1694773b431c1e549951827392abcae
Bayram Çınar <bayram200212@gmail.com>
2024-05-16 12:00:59 +0300

    agoraTest

--- app/live/[ChName]/page.jsx
+++ app/live/[ChName]/page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import Room from "@/components/LiveComponents/Room";
 
 const LiveContainer = dynamic(() => import("@/components/LiveComponents"), {
   ssr: false,
 });
 
 function Page() {
-  return <LiveContainer />;
+  return <Room />;
 }
 
 export default Page;
--- app/live/page.jsx
+++ app/live/page.jsx
@@ -1,82 +1,106 @@
 "use client";
-import React from "react";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { getAPI } from "@/services/fetchAPI";
 
 function Page() {
-  const [token, setToken] = React.useState("");
-  const [channel, setChannel] = React.useState("");
-  const [uid, setUid] = React.useState("");
+  const router = useRouter();
 
-  const handleSubmit = async (e) => {
+  const [roomLogin, setRoomLogin] = useState({
+    username: sessionStorage.getItem("username") || "",
+    roomName: "",
+  });
+  const [token, setToken] = useState("");
+  const [channel, setChannel] = useState("");
+  const [uid, setUid] = useState("");
+  const [showCreateRoom, setShowCreateRoom] = useState(true);
+
+  const closeRoomModal = () => setShowCreateRoom(false);
+
+  const handleRoomSubmit = async (e) => {
     e.preventDefault();
-    const enteredChannel = e.target.channel.value;
-    const enteredUid = e.target.name.value;
+    sessionStorage.setItem("username", String(roomLogin.username));
+
     try {
       const response = await getAPI(
-        `/agora?channelName=${enteredChannel}&uid=${enteredUid}`
+        `/agora?channelName=${roomLogin.roomName}&uid=${roomLogin.username}`
       );
       const token = response.token;
-      setChannel(enteredChannel);
+      setChannel(roomLogin.roomName);
       setToken(token);
-      setUid(enteredUid);
+      setUid(roomLogin.username);
+
+      router.push(`/live/${roomLogin.roomName}`);
+      closeRoomModal();
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="mb-4 mt-20 text-4xl font-extrabold leading-none tracking-tight text-gray-900">
-        <span className="text-premiumOrange">Ofistik</span> x{" "}
-        <span className="text-blue-500">Agora</span>
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <div className="md:flex md:items-center mt-6">
-          <div>
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
+    <>
+      {showCreateRoom && (
+        <div className="fixed z-20 w-screen h-screen top-0 left-0 flex items-center justify-center bg-slate-800/50">
+          <div className="w-[98vw] h-auto max-h-screen overflow-y-auto border border-orange-500 bg-slate-900 rounded-lg md:w-[500px] md:h-[400px]">
+            <div className="py-3 bg-slate-800 flex items-center justify-between">
+              <b className="text-lg w-[70%] text-end font-bold">
+                Toplantı Başlat
+              </b>
+              <b className="w-[30%] pe-2 text-end">
+                <span onClick={closeRoomModal} className="cursor-pointer">
+                  X
+                </span>
+              </b>
+            </div>
+            <form
+              onSubmit={handleRoomSubmit}
+              className="px-6 py-10 flex flex-col gap-4"
             >
-              Channel Name
-            </label>
-          </div>
-          <div>
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="inline-full-name"
-              type="text"
-              name="channel"
-              placeholder="Enter channel name"
-              required
-            />
+              <div className="flex w-full flex-col gap-2">
+                <label htmlFor="name">İsminiz</label>
+                <input
+                  type="text"
+                  className="h-[44px] bg-slate-800 rounded-lg p-3 focus:outline-none focus:border focus:border-orange-500"
+                  value={roomLogin.username}
+                  onChange={(e) =>
+                    setRoomLogin((prev) => ({
+                      ...prev,
+                      username: e.target.value,
+                    }))
+                  }
+                  required
+                />
+              </div>
+              <div className="flex w-full flex-col gap-2">
+                <label htmlFor="roomName">Oda Numaranız</label>
+                <input
+                  type="text"
+                  className="h-[44px] bg-slate-800 rounded-lg p-3 focus:outline-none focus:border focus:border-orange-500"
+                  value={roomLogin.roomName}
+                  onChange={(e) =>
+                    setRoomLogin((prev) => ({
+                      ...prev,
+                      roomName: e.target.value,
+                    }))
+                  }
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="mt-3 rounded-md bg-orange-500 px-3 py-3 font-bold"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(to right, #ff8c00, #ff2f96)",
+                }}
+              >
+                Başlat
+              </button>
+            </form>
           </div>
         </div>
-        <div className="md:flex md:items-center mt-6">
-          <div>
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              User Name
-            </label>
-          </div>
-          <div>
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="inline-full-name"
-              type="text"
-              name="name"
-              placeholder="Enter user name"
-              required
-            />
-          </div>
-        </div>
-        <div className="text-center">
-          <button className="inline-flex items-center justify-center px-5 py-3 mt-5 text-base font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-            Submit
-          </button>
-        </div>
-      </form>
+      )}
       {token && (
         <div className="m-10 w-96 flex flex-wrap">
           <a
@@ -87,7 +111,7 @@ function Page() {
           </a>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
