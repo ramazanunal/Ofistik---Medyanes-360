76a910fffb0b3c708d4b43944ad8e0e18eebb652
Bayram Çınar <bayram200212@gmail.com>
2024-06-30 17:51:46 +0300

    fix

--- components/LiveComponents/whiteBoardMain.jsx
+++ components/LiveComponents/whiteBoardMain.jsx
@@ -2,28 +2,24 @@ import { useFastboard, Fastboard } from "@netless/fastboard-react";
 import React, { useEffect, useState } from "react";
 
 function WhiteBoardMain({ roomToken, uid, uuid, showChat, showParticipants }) {
-  const [fastboard, setFastboard] = useState(null);
   const [size, setSize] = useState("");
 
-  useEffect(() => {
-    // Fastboard başlatma işlemini burada güvenli bir şekilde gerçekleştirin
-    try {
-      const fb = useFastboard(() => ({
-        sdkConfig: {
-          appIdentifier: "PFDmUAhSEe-X_REUu_elzA/kmPn7ALITrakJQ",
-          region: "us-sv",
-        },
-        joinRoom: {
-          uid: uid,
-          uuid: uuid,
-          roomToken: roomToken,
-        },
-      }));
-      setFastboard(fb);
-    } catch (error) {
-      console.error("An error occurred while initializing fastboard:", error);
-    }
-  }, [roomToken, uid, uuid]);
+  let fastboard;
+  try {
+    fastboard = useFastboard(() => ({
+      sdkConfig: {
+        appIdentifier: "PFDmUAhSEe-X_REUu_elzA/kmPn7ALITrakJQ",
+        region: "us-sv",
+      },
+      joinRoom: {
+        uid: uid,
+        uuid: uuid,
+        roomToken: roomToken,
+      },
+    }));
+  } catch (error) {
+    console.error("An error occurred while initializing fastboard:", error);
+  }
 
   useEffect(() => {
     // showChat ve showParticipants durumlarına göre genişliği ayarlayın
@@ -62,7 +58,7 @@ function WhiteBoardMain({ roomToken, uid, uuid, showChat, showParticipants }) {
         background: "#f1f2f3",
       }}
     >
-      {fastboard && <Fastboard app={fastboard} />}
+      <Fastboard app={fastboard} />
     </div>
   );
 }
